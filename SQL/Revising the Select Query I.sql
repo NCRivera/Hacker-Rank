7cf023f1b41a85107b1f0e734d7fd42bc9a0c459
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-10-28 19:20:21 -0400

    Add files via upload

---
+++ SQL/Revising the Select Query I.sql
@@ -0,0 +1,11 @@
+SELECT * FROM city WHERE CountryCode = 'USA' AND Population > 100000;
+
+/*
+Output:
+
+  3878 Scottsdale USA Arizona 202705
+  3965 Corona USA California 124966
+  3973 Concord USA California 121780
+  3977 Cedar Rapids USA Iowa 120758
+  3982 Coral Springs USA Florida 117549
+ */
